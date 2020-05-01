cbbdb94ff84ec666fcaf8f257d76aab70d89fecc
noheliaeuceda <noheliaeucedaf@gmail.com>
2020-05-01 14:04:37 -0600

    validacion

--- P3Lab1_PaulinaEuceda.cpp
+++ P3Lab1_PaulinaEuceda.cpp
@@ -2,7 +2,21 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
+using namespace std;
+
 int main(int argc, char** argv) {
-	//holi
-	return 0;
+	//que
+	int numeroIngresado;
+	
+	cout << "Ingrese un numero: ";
+	cin >> numeroIngresado;
+	
+	if(numeroIngresado < 100){
+		cout << "funciona";
+	}
+	else{
+		cout << "Debe ser menor que 100. Gracias";
+	}
+	
+		return 0;
 }
